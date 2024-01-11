7cfd1be2a9300832f840a132bda3baf5528399f6
salih-kv <profileofsalih@gmail.com>
2024-01-11 13:25:32 +0530

    complete breakdown page functionalities

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import Geography from "./scenes/geography";
 import Overview from "./scenes/overview";
 import Daily from "./scenes/daily";
 import Monthly from "./scenes/monthly";
+import Breakdown from "./scenes/breakdown";
 
 export const App = () => {
   const mode = useSelector((state) => state.global.mode);
@@ -33,6 +34,7 @@ export const App = () => {
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
               <Route path="/monthly" element={<Monthly />} />
+              <Route path="/breakdown" element={<Breakdown />} />
             </Route>
           </Routes>
         </ThemeProvider>
