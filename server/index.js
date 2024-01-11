e6c16a35a8d24e1a4049cdae338cb7abd6a0ebe3
salih-kv <profileofsalih@gmail.com>
2024-01-11 14:42:56 +0530

    implement admins page functionalities

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import clientRoutes from "./routes/client.js";
 import generalRoutes from "./routes/general.js";
-// import managementRoutes from "./routes/management.js";
+import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
 // DATA IMPORTS
@@ -31,12 +31,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-// app.use(morgan());
+app.use(morgan("common"));
 
 // ROUTES
 app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
-// app.use("/management", managementRoutes);
+app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
 
 // MONGOOSE SETUP
